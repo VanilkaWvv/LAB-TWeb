d1165714f84a5e4820f5ecf06ff06105ce727fa9
VanilkaWvv <valentinprepelita3@gmail.com>
2024-02-15 10:25:29 +0200

    Laboratorul_2

---
+++ untitled/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p id="nume">
+            Prepelița Valentin
+        </p>
+          <p id="grupa">
+              CR-222
+          </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
