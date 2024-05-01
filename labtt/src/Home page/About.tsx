1dc6593d9440d23c41a3d86b5200377a44c6cc68
VanilkaWvv <valentinprepelita3@gmail.com>
2024-05-02 00:00:43 +0300

    lab7

---
+++ labtt/src/Home page/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./about.css";
+const About = () => {
+    return (
+        <div>
+            <h1>Salutare, multumesc ca ai accesat acest link</h1>
+            <p>Aceasta pagina este creata pentru a fi o descriere a creatorului.</p>
+                <p>Creatorul este:</p>
+                <p>Prepelita Valentin</p>
+                <p>Grupa CR-222</p>
+                <p>Universitatea tehnica a Moldovei</p>
+            <p>Anul 2024</p>
+        </div>
+    );
+};
+
+export default About;
